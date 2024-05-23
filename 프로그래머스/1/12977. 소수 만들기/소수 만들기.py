4aaf776c2010701cfa91cd90495a4852adf3bc05
wodm15 <92417963+wodm15@users.noreply.github.com>
2024-05-23 09:36:20 +0900

    [level 1] Title: 소수 만들기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
@@ -0,0 +1,20 @@
+from itertools import permutations ,combinations
+
+def solution(nums):
+    answer = 0
+    
+    for comb in combinations(nums, 3):
+        s = sum(comb)
+        if is_prime_number(s):
+            answer += 1
+            
+    return answer
+        
+
+def is_prime_number(x):
+    if x < 2:
+        return False
+    for i in range(2, int(x**0.5) + 1):
+        if x % i == 0:
+            return False
+    return True
