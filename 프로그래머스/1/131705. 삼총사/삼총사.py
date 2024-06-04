61a20f2d49eedaa4786ef5263a986f71f7af1339
wodm15 <92417963+wodm15@users.noreply.github.com>
2024-06-04 09:56:33 +0900

    [level 1] Title: 삼총사, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/131705. 삼총사/삼총사.py
@@ -0,0 +1,10 @@
+def solution(number):
+    from itertools import combinations
+    cnt=0
+    for i in combinations(number,3):
+        if sum(i)==0:
+            cnt=cnt+1
+
+            
+    
+    return cnt
