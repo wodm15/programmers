31cbeb7a70bcf7566306da115f0350fe53b8f9a4
wodm15 <92417963+wodm15@users.noreply.github.com>
2024-09-04 15:01:37 +0900

    [Silver V] Title: 최소공배수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/13241. 최소공배수/최소공배수.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+long long gcd(long long a, long long b) {
+    while (b != 0) {
+        long long temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+long long lcm(long long a, long long b) {
+    return (a * b) / gcd(a, b);
+}
+
+int main() {
+    long long A, B;
+    cin >> A >> B;  
+    cout << lcm(A, B) << '\n';
+
+    return 0;
+}
