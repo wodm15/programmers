9edfcd526d3a522b5e8149142c5c05778de5a9f7
wodm15 <92417963+wodm15@users.noreply.github.com>
2024-09-06 15:38:47 +0900

    [Silver V] Title: 창문 닫기, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath> 
+
+using namespace std;
+
+int main() {
+    long long N;
+    cin >> N;
+
+    cout << static_cast<long long>(sqrt(N)) << endl;
+
+    return 0;
+}
