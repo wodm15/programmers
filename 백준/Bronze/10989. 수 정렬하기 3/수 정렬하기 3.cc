0aa71947501f52cf201cd54ed54a5d57c2e26600
wodm15 <92417963+wodm15@users.noreply.github.com>
2024-10-11 17:36:17 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 4220 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+int main() {
+
+    int N;
+    cin >> N;
+
+    int count[10001] = {0};
+    int number = 0;
+
+    for(int i=0 ; i< N; ++i)
+        {
+            cin >> number;
+            count[number]++;
+        }
+
+    for(int i=0; i<= 10000 ; ++i)
+        {
+            if(count[i]!= 0 ){
+                for(int j=0; j < count[i] ; ++j)
+                    cout << i << '\n';
+            }
+        }
+    
+}
